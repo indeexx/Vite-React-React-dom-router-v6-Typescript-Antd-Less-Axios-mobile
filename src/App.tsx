815a84253b8057874baf43b37f4304edf37ce086
indeex <indeexx@users.noreply.github.com>
2022-03-22 13:41:02 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { ReactNode, Suspense } from "react";
 import { useRoutes } from 'react-router-dom';
 import Routes from "./router/routes"
+
+const lazyLoad = (children: ReactNode): ReactNode => {
+  return <Suspense fallback={<h1>一个特别好看的Loading...</h1>}>
+    {children}
+  </Suspense>
+}
 
 const App: React.FC = (): JSX.Element => {
   let routes: any = [];
@@ -8,7 +14,7 @@ const App: React.FC = (): JSX.Element => {
     const Ele = route.element;
     const router = {
       path: route.path,
-      element: <Ele />,
+      element: lazyLoad(<Ele />),
     };
     routes.push(router);
   });
